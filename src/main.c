f8ff5014aa5d5050c570777f17cf5dc15c86269f
Mohamed Regrag <mregrag@e3r3p5.1337.ma>
2024-08-26 10:29:01 +0100

    change 3

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mregrag <mregrag@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 18:21:10 by mregrag           #+#    #+#             */
-/*   Updated: 2024/08/26 10:19:12 by mregrag          ###   ########.fr       */
+/*   Updated: 2024/08/26 10:28:09 by mregrag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ int	main(int argc, char **argv)
 
 	if (argc > 1)
 		parsing_map(argv[1], &map);
-
 	printf("change");
+	printf("change 3");
 	start_the_game(&map);
 	return (EXIT_SUCCESS);
 }
